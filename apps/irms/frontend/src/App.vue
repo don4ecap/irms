ea627482a798ad81e35130b3f037fa463a4816e0
Don Alfons Nisnoni <don@kronosinvestments.net>
2022-10-04 16:19:04 +0800

    vue basic setup on irms
    Signed-off-by: Don Alfons Nisnoni <don@kronosinvestments.net>

---
+++ apps/irms/frontend/src/App.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      h1: 'Hey there...',
+      text: 'Hello world',
+    }
+  },
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ h1 }}</h1>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: blueviolet;
+}
+</style>
