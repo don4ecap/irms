7d120242329d2dbb07895147efff3b5ee551b369
Don Alfons Nisnoni <don@kronosinvestments.net>
2022-10-05 15:23:58 +0800

    Added eslint and its config
    Signed-off-by: Don Alfons Nisnoni <don@kronosinvestments.net>

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+const production = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  plugins: ['vue', 'prettier', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-debugger': production ? 2 : 0,
+    'no-unused-vars': production ? 2 : 1,
+  },
+}
