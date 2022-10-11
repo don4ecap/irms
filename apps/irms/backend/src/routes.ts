fdcd63d4b4a795f69516553264419e60aff0a36f
Don <don@kronosinvesments.net>
2022-10-11 10:38:45 +0800

    [irms-be] Added get portfolio API endpoint
    Signed-off-by: Don <don@kronosinvesments.net>

--- apps/irms/backend/src/routes.ts
+++ apps/irms/backend/src/routes.ts
@@ -40,6 +40,38 @@ const routes: Array<RouteOptions> = [
     },
   },
 
+  {
+    method: 'GET',
+    url: `${prefix}/get_portfolio/:account/:trade_date`,
+    handler(req, res) {
+      db.then((connection) => {
+        const params: GetNavRequestParams = req.params as GetNavRequestParams
+        connection
+          .query(
+            `SELECT 
+              SUM(positions_pct_target) AS positions_pct_target, 
+              SUM(qty) AS qty, 
+              SUM(current_allocation_pct) AS current_allocation_pct, 
+              SUM(current_allocation_lots) AS current_allocation_lots, 
+              SUM(target_allocation_pct) AS target_allocation_pct, 
+              SUM(target_allocation_lots) AS target_allocation_lots, 
+              SUM(current_risks_pre) AS current_risks_pre, 
+              SUM(target_risks_pre) AS target_risks_pre 
+            FROM 
+              irms 
+            WHERE 
+              account = '${params.account}' 
+              AND td = '${params.trade_date}' 
+              AND rowtype = 'sector' 
+              AND instrument <> 'cash'`
+          )
+          .then((rows) => {
+            return res.send(rows[0])
+          })
+      })
+    },
+  },
+
   // {
   //   method: 'GET',
   //   url: `${prefix}/test`,
