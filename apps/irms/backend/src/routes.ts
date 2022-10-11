2b5fb5f44447184fa643de2e07c11fae7b31ee36
Don <don@kronosinvesments.net>
2022-10-11 10:01:42 +0800

    [irms-be] Added get book API endpoint
    Signed-off-by: Don <don@kronosinvesments.net>

--- apps/irms/backend/src/routes.ts
+++ apps/irms/backend/src/routes.ts
@@ -23,6 +23,23 @@ const routes: Array<RouteOptions> = [
     },
   },
 
+  {
+    method: 'GET',
+    url: `${prefix}/get_book/:account/:trade_date`,
+    handler(req, res) {
+      db.then((connection) => {
+        const params: GetNavRequestParams = req.params as GetNavRequestParams
+        connection
+          .query(
+            `SELECT * FROM irms WHERE irms.account='${params.account}' AND td='${params.trade_date}' ORDER BY irms.orderNo, irms.year DESC, irms.month DESC`
+          )
+          .then((rows) => {
+            return res.send(rows)
+          })
+      })
+    },
+  },
+
   // {
   //   method: 'GET',
   //   url: `${prefix}/test`,
