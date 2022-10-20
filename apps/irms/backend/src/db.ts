63e8d7d8a1c6bef31e87f34abaff77ae3c5bfed5
Don <don@kronosinvestments.net>
2022-10-20 19:12:14 +0800

    Use mariadb pool connection
    Signed-off-by: Don <don@kronosinvestments.net>

--- apps/irms/backend/src/db.ts
+++ apps/irms/backend/src/db.ts
@@ -1,11 +1,12 @@
 import mariadb from 'mariadb'
 
-const db = mariadb.createConnection({
+const db = mariadb.createPool({
   host: '10.153.64.65',
   user: 'root',
   port: 3304,
   password: 'FOUR2008',
   decimalAsNumber: true,
+  connectionLimit: 5,
 })
 
 export default db
--- apps/irms/backend/src/routes.ts
+++ apps/irms/backend/src/routes.ts
@@ -1,6 +1,6 @@
 import { FastifyReply, RouteOptions } from 'fastify'
 import type { AccountOnlyParams, GetNavRequestParams } from './types'
-import dbConnection from './db'
+import db from './db'
 
 const prefix = '/api'
 
@@ -15,17 +15,22 @@ const routes: Array<RouteOptions> = [
     method: 'GET',
     url: `${prefix}/get_nav/:account/:trade_date`,
     handler(req, res) {
-      dbConnection.then((connection) => {
-        const params: GetNavRequestParams = req.params as GetNavRequestParams
-        connection
-          .query(
-            `SELECT * FROM trading.tblfonav WHERE account='${params.account}' AND td <= '${params.trade_date}' ORDER BY TIMESTAMP DESC LIMIT 1;`
-          )
-          .then((rows) => {
-            return res.send(rows[0] || [])
-          })
-          .catch(internalServerErrorHandler(res))
-      })
+      db.getConnection()
+        .then((connection) => {
+          const params: GetNavRequestParams = req.params as GetNavRequestParams
+          connection
+            .query(
+              `SELECT * FROM trading.tblfonav WHERE account='${params.account}' AND td <= '${params.trade_date}' ORDER BY TIMESTAMP DESC LIMIT 1;`
+            )
+            .then((rows) => {
+              return res.send(rows[0] || [])
+            })
+            .catch(internalServerErrorHandler(res))
+            .finally(() => {
+              connection.end()
+            })
+        })
+        .catch(internalServerErrorHandler(res))
     },
   },
 
@@ -33,17 +38,22 @@ const routes: Array<RouteOptions> = [
     method: 'GET',
     url: `${prefix}/get_book/:account/:trade_date`,
     handler(req, res) {
-      dbConnection.then((connection) => {
-        const params: GetNavRequestParams = req.params as GetNavRequestParams
-        connection
-          .query(
-            `SELECT * FROM trading.irms WHERE irms.account='${params.account}' AND td='${params.trade_date}' ORDER BY irms.orderNo, irms.year ASC, irms.month ASC`
-          )
-          .then((rows) => {
-            return res.send(rows)
-          })
-          .catch(internalServerErrorHandler(res))
-      })
+      db.getConnection()
+        .then((connection) => {
+          const params: GetNavRequestParams = req.params as GetNavRequestParams
+          connection
+            .query(
+              `SELECT * FROM trading.irms WHERE irms.account='${params.account}' AND td='${params.trade_date}' ORDER BY irms.orderNo, irms.year ASC, irms.month ASC`
+            )
+            .then((rows) => {
+              return res.send(rows)
+            })
+            .catch(internalServerErrorHandler(res))
+            .finally(() => {
+              connection.end()
+            })
+        })
+        .catch(internalServerErrorHandler(res))
     },
   },
 
@@ -51,11 +61,12 @@ const routes: Array<RouteOptions> = [
     method: 'GET',
     url: `${prefix}/get_portfolio/:account/:trade_date`,
     handler(req, res) {
-      dbConnection.then((connection) => {
-        const params: GetNavRequestParams = req.params as GetNavRequestParams
-        connection
-          .query(
-            `SELECT 
+      db.getConnection()
+        .then((connection) => {
+          const params: GetNavRequestParams = req.params as GetNavRequestParams
+          connection
+            .query(
+              `SELECT 
               SUM(positions_pct_target) AS positions_pct_target, 
               SUM(qty) AS qty, 
               SUM(current_allocation_pct) AS current_allocation_pct, 
@@ -71,12 +82,16 @@ const routes: Array<RouteOptions> = [
               AND td = '${params.trade_date}' 
               AND rowtype = 'sector' 
               AND instrument <> 'cash'`
-          )
-          .then((rows) => {
-            return res.send(rows[0] || [])
-          })
-          .catch(internalServerErrorHandler(res))
-      })
+            )
+            .then((rows) => {
+              return res.send(rows[0] || [])
+            })
+            .catch(internalServerErrorHandler(res))
+            .finally(() => {
+              connection.end()
+            })
+        })
+        .catch(internalServerErrorHandler(res))
     },
   },
 
@@ -84,11 +99,12 @@ const routes: Array<RouteOptions> = [
     method: 'GET',
     url: `${prefix}/check_last_calculated/:account`,
     handler(req, res) {
-      dbConnection.then((connection) => {
-        const params: AccountOnlyParams = req.params as AccountOnlyParams
-        connection
-          .query(
-            `SELECT
+      db.getConnection()
+        .then((connection) => {
+          const params: AccountOnlyParams = req.params as AccountOnlyParams
+          connection
+            .query(
+              `SELECT
               value
             FROM
               cacheDB.cache2
@@ -96,12 +112,16 @@ const routes: Array<RouteOptions> = [
               name
             LIKE 'irms_calculate_${params.account}'
             LIMIT 1`
-          )
-          .then((rows) => {
-            return res.send(rows[0] || [])
-          })
-          .catch(internalServerErrorHandler(res))
-      })
+            )
+            .then((rows) => {
+              return res.send(rows[0] || [])
+            })
+            .catch(internalServerErrorHandler(res))
+            .finally(() => {
+              connection.end()
+            })
+        })
+        .catch(internalServerErrorHandler(res))
     },
   },
 
@@ -109,16 +129,21 @@ const routes: Array<RouteOptions> = [
     method: 'GET',
     url: `${prefix}/get_commo_indicator_level`,
     handler(req, res) {
-      dbConnection.then((connection) => {
-        connection
-          .query(
-            `SELECT contract, maxLevel FROM customRef.execution_commoindicatormaxlevel_ees_live`
-          )
-          .then((rows) => {
-            return res.send(rows)
-          })
-          .catch(internalServerErrorHandler(res))
-      })
+      db.getConnection()
+        .then((connection) => {
+          connection
+            .query(
+              `SELECT contract, maxLevel FROM customRef.execution_commoindicatormaxlevel_ees_live`
+            )
+            .then((rows) => {
+              return res.send(rows)
+            })
+            .catch(internalServerErrorHandler(res))
+            .finally(() => {
+              connection.end()
+            })
+        })
+        .catch(internalServerErrorHandler(res))
     },
   },
 
@@ -126,14 +151,19 @@ const routes: Array<RouteOptions> = [
     method: 'GET',
     url: `${prefix}/get_strategies`,
     handler(req, res) {
-      dbConnection.then((connection) => {
-        connection
-          .query(`SELECT strategy_name from trading.ie_strategy`)
-          .then((rows) => {
-            return res.send(rows)
-          })
-          .catch(internalServerErrorHandler(res))
-      })
+      db.getConnection()
+        .then((connection) => {
+          connection
+            .query(`SELECT strategy_name from trading.ie_strategy`)
+            .then((rows) => {
+              return res.send(rows)
+            })
+            .catch(internalServerErrorHandler(res))
+            .finally(() => {
+              connection.end()
+            })
+        })
+        .catch(internalServerErrorHandler(res))
     },
   },
 
