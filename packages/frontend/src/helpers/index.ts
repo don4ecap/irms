7063fed35993445f1ff2a9921586bea3d4837f8b
Don <don@kronosinvestments.net>
2023-01-11 11:09:41 +0800

    Hotfix live pnl status & live pnl decoration
    Signed-off-by: Don <don@kronosinvestments.net>

--- packages/frontend/src/helpers/index.ts
+++ packages/frontend/src/helpers/index.ts
@@ -8,7 +8,7 @@ function formatNavData(nav: any, account: string) {
     live_nav: accounting.formatMoney(parseFloat(nav.live_nav), symbol),
     subred: accounting.formatMoney(nav.subred, symbol),
     live_pnl_decorated: `${accounting.formatMoney(nav.live_pnl, symbol)} 
-              (${((nav.live_pnl_pct || 0) * 100).toFixed(2)}%)`,
+              (+${((nav.live_pnl_pct || 0) * 100).toFixed(2)}%)`,
     last_nav: accounting.formatMoney(nav.last_nav, symbol),
     //@ts-ignore
     last_calculated: moment(nav.timestamp).format('LLL'),
@@ -18,7 +18,7 @@ function formatNavData(nav: any, account: string) {
     // last_nav_estimated: parseInt(nav.last_nav_estimated),
 
     // New data
-    live_pnl_status: nav.last_nav_estimated ? 'reconciled' : 'estimated',
+    live_pnl_status: nav.last_nav_estimated ? 'estimated' : 'reconciled',
     live_pnl_status_title: `Last PNL: ${accounting.formatMoney(
       nav.last_pnl,
       symbol
