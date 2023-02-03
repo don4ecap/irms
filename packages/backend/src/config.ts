0b00ce3a87ffa3d5282e5cccedb9e601f0cf8ba5
Don <don@kronosinvestments.net>
2023-02-03 15:37:50 +0800

    Add config: Path to config file
    Signed-off-by: Don <don@kronosinvestments.net>

--- packages/backend/src/config.ts
+++ packages/backend/src/config.ts
@@ -1,10 +1,13 @@
 import fs from 'fs'
 import { ConfigIniParser } from 'config-ini-parser'
 
+const configFilePath =
+  process.env.IRMS_BACKEND_CONFIG_FILE ||
+  '/mnt/public/GLibs/FourElementsConfig/iRMS/config.cfg'
+
 function getConfig(fieldName: string): string {
-  const configContent = fs
-    .readFileSync('/mnt/public/GLibs/FourElementsConfig/iRMS/config.cfg')
-    .toString('utf8')
+  console.log('Load config file from', configFilePath)
+  const configContent = fs.readFileSync(configFilePath).toString('utf8')
 
   const parser = new ConfigIniParser()
   parser.parse(configContent)
